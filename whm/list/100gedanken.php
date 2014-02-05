456c712d0c3ed3639dc19b0d86e3c3c23147da1a
Nils Langner <nils.langner@phmLabs.com>
2014-02-05 09:39:07 +0100

    removed author from 100 gedanken ad

--- whm/list/100gedanken.php
+++ whm/list/100gedanken.php
@@ -1,7 +1,6 @@
 <article id="post-<?php the_ID(); ?>" class="item">
         <a target="_blank" href="http://www.amazon.de/gp/product/3868020888?ie=UTF8&camp=3206&creative=21426&creativeASIN=3868020888&linkCode=shr&tag=phhamebuthsok-21" rel="bookmark"><img src="/images/100gedanken.png" width="100%"></a>
         <div class="whm-post-info">
-            <div class="autor"><img src="/images/autoren/Nils-Langner-small.png" /></div>
             <h2 class="entry-title"><a target="_blank" href=http://www.amazon.de/gp/product/3868020888?ie=UTF8&camp=3206&creative=21426&creativeASIN=3868020888&linkCode=shr&tag=phhamebuthsok-21" rel="bookmark">Das Buch zum Blog.</a></h2>
             <div class="entry-content">
     		  <p>Der Leser wird durch den gesamten Softwareentwicklungsprozess begleitet und in jedem Kapitel an einen Punkt gebracht, an dem er selbst&auml;ndig weiterdenken kann.</p>
